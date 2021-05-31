cde535b2195947c67f01ba21864be4cea8dc1dd9
Raunak <rawnakkapoor@gmail.com>
2021-05-31 18:34:43 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello "+ name + " Have a good day");
+
+    }
+}
